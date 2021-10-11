212a03707adf4b4ce7da8b26196be27fcd0ce678
Saulo <saulo.monteiro.dev@gmail.com>
2021-10-11 11:21:46 -0300

    form completo usando data driven

--- src/app/data-driven/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven/data-driven.component.ts
@@ -1,5 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { Form, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  Form,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven',
@@ -7,11 +15,9 @@ import { Form, FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./data-driven.component.scss'],
 })
 export class DataDrivenComponent implements OnInit {
-
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   form: FormGroup;
-
 
   ngOnInit(): void {
     /*
@@ -20,10 +26,31 @@ export class DataDrivenComponent implements OnInit {
       email: new FormControl(null),
     });
     */
-   this.form = this.formBuilder.group({
-     nome: [null],
-     email: [null]
-   })
-   console.log(this.form);
+    this.form = this.formBuilder.group({
+      nome: ['Saulo', Validators.required],
+      email: ['saul0kz@gmail.com', [Validators.required, Validators.email]],
+      cep:  ['49042570', Validators.required ],
+      numero:  [42, Validators.required],
+      complemento:  [null],
+      rua:  ['323', Validators.required],
+    });
+    console.log(this.form);
+  }
+  submit() {
+    let valueSubmit = Object.assign({}, this.form.value);
+
+    this.http
+      .post('https://httpbin.org/post', JSON.stringify(valueSubmit))
+      .subscribe(
+        (dados) => {
+          console.log(dados);
+          //    this.form.reset();
+        },
+        (error: any) => alert('erro')
+      );
+  }
+
+  cancelar() {
+    this.form.reset();
   }
 }
